6f3707bc311b4683d9eb7e0ffcb6f6681e4db388
FABIO RONDINA <1707082@rgu.ac.uk>
2019-08-05 11:04:06 +0100

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,12 @@
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var txt = new Text();
+                txt.Width = 100;
+                txt.Copy();
+        }
+    }
+}
